052e5b75f5fc3d1a06d6ebfd9b534b63b5c56cfa
Humphrey Mugambi <mugagambi@gmail.com>
2018-05-21 11:58:26 +0300

    added vue bootstrap and a navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+<b-navbar toggleable="sm" type="light" variant="light">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand href="#">Vue Pouchdb PWA</b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav>
+      <b-nav-item to="/" exact>Home</b-nav-item>
+      <b-nav-item to="/about" exact>About</b-nav-item>    
+    </b-navbar-nav>  
+  </b-collapse>
+</b-navbar>
+</template>
+<style lang="scss" scoped>
+
+</style>
+
